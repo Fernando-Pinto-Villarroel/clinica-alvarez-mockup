36a707730130035f5f5f83d39986f42ceca745e0
Fernando Pinto Villarroel <Fernando.Pinto@jala.university>
2025-05-31 18:32:54 -0400

    Deploy website

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = "clinica-alvarez-mockup";
+
 const nextConfig = {
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +15,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
